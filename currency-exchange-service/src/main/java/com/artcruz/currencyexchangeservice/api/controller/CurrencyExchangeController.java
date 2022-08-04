4b628069f532bde2aa9e98daa64b3b4ef9d13382
Arthur Cruz <arthur.silva1041@gmail.com>
2022-08-04 00:52:11 -0300

    adding distributed tracing with zipkin

--- currency-exchange-service/src/main/java/com/artcruz/currencyexchangeservice/api/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/artcruz/currencyexchangeservice/api/controller/CurrencyExchangeController.java
@@ -4,6 +4,8 @@ import com.artcruz.currencyexchangeservice.api.response.CurrencyExchangeResponse
 import com.artcruz.currencyexchangeservice.model.entity.CurrencyExchange;
 import com.artcruz.currencyexchangeservice.model.repository.CurrencyExchangeRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 public class CurrencyExchangeController {
-
     private final CurrencyExchangeRepository repository;
-
     private final Environment environment;
-
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchangeResponse retrieveExchangeValue(
             @PathVariable(name = "from") String originCurrency,
